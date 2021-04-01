e3fb985b99e5b953c0a7245eff3ed9f3a773b15f
taechit.c <taechit.c@gmail.com>
2021-04-01 16:18:25 +0700

    update verify function

--- signature.js
+++ signature.js
@@ -1,30 +1,32 @@
 const rs = require('jsrsasign')
-const rsu = require('jsrsasign-util')
 
 module.exports.generate = function (privateKey,data){
     const sig = new rs.KJUR.crypto.Signature({alg: 'SHA256withRSA'})
     sig.init(privateKey)
     sig.updateString(data)
     const hSigVal = sig.sign()
-    const base64String = hexToBase64(hSigVal)
-    const signature = "digest-alg=RSA-SHA; key-id=KEY:RSA:rsf.org; data=" + base64String
+    console.log("Before hex",hSigVal);
+
+    const base64String = Buffer.from(hSigVal,"hex").toString('base64')
+    const signature = base64String
     console.log("signature",signature)
     return signature
 }
 
-module.exports.verify = function (publicKey,sigHex,data){
+module.exports.verify = function (publicKey,sign,data){
     const sig = new rs.KJUR.crypto.Signature({alg: 'SHA256withRSA'})
-    const value = rsu.readFile(data)
     sig.init(publicKey)
-    sig.updateString(value)
+    sig.updateString(data)
+    const sigHex = Buffer.from(sign,"base64").toString("hex")
+    console.log("Before verify",sigHex);
+
     const isValid = sig.verify(sigHex)
+    if (isValid) {
+        console.log("signature is valid");
+    } else {
+        console.log("signature is invalid");
+    }
     return isValid
 }
 
-function hexToBase64(hexString) {
-    return Buffer.from(hexString.match(/\w{2}/g).map(function(a) {
-             return String.fromCharCode(parseInt(a, 16))
-         }).join("")).toString('base64')
-
-}
 
