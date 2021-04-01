f231676dacb1b7747ad98ea8afe6334ab878cfac
taechit.cha <taechit.cha@ascendcorp.com>
2021-04-01 14:13:32 +0700

    refactor code

--- signature.js
+++ signature.js
@@ -1,28 +1,28 @@
 const rs = require('jsrsasign');
 const rsu = require('jsrsasign-util');
 
-function generate(privateKey,data){
-    const sig = new rs.KJUR.Signature({alg: 'SHA256withRSA'});
-    sig.init(privateKey);
-    sig.updateString(data);
-    const hSigVal = sig.sign();
-    const base64String = hexToBase64(hSigVal);
+export function generate(privateKey,data){
+    const sig = new rs.KJUR.Signature({alg: 'SHA256withRSA'})
+    sig.init(privateKey)
+    sig.updateString(data)
+    const hSigVal = sig.sign()
+    const base64String = hexToBase64(hSigVal)
     const signature = "digest-alg=RSA-SHA; key-id=KEY:RSA:rsf.org; data=" + base64String
-    console.log("signature",signature);
-    return signature;
+    console.log("signature",signature)
+    return signature
 }
 
-function verify(publicKey,sigHex,data){
-    const sig = new rs.KJUR.Signature({alg: 'SHA256withRSA'});
-    const value = rsu.readFile(data);
-    sig.init(publicKey);
-    sig.updateString(value);
-    const isValid = sig.verify(sigHex);
-    return isValid;
+export function verify(publicKey,sigHex,data){
+    const sig = new rs.KJUR.Signature({alg: 'SHA256withRSA'})
+    const value = rsu.readFile(data)
+    sig.init(publicKey)
+    sig.updateString(value)
+    const isValid = sig.verify(sigHex)
+    return isValid
 }
 
 function hexToBase64(hexString) {
     return btoa(hexString.match(/\w{2}/g).map(function(a) {
-        return String.fromCharCode(parseInt(a, 16));
-    }).join(""));
+        return String.fromCharCode(parseInt(a, 16))
+    }).join(""))
 }
